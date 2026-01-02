07e6b64bc9d9ace062f7b7934c95a0fa62744738
jinhyo <kjhyo0220@gmail.com>
2026-01-02 16:50:59 +0900

    Feat: 폴더 구조 및 절대 경로 설정 (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,20 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), tailwindcss()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@apis', replacement: '/src/apis' },
+      { find: '@assets', replacement: '/src/assets' },
+      { find: '@constants', replacement: '/src/constants' },
+      { find: '@data', replacement: '/src/data' },
+      { find: '@feature', replacement: '/src/feature' },
+      { find: '@layouts', replacement: '/src/layouts' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@routes', replacement: '/src/routes' },
+      { find: '@shared', replacement: '/src/shared' },
+      { find: '@stores', replacement: '/src/stores' },
+      { find: '@types', replacement: '/src/types' },
+    ],
+  },
 });
