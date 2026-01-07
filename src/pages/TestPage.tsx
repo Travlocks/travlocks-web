736fb5dc8c8de2b3d6f7a79f6fc4711bec8b8b5f
jinhyo <kjhyo0220@gmail.com>
2026-01-08 04:18:27 +0900

    Feat: RoundButton.tsx 컴포넌트 구현 (#3)

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,4 +1,5 @@
 import Button from '@/shared/components/Button/Button';
+import RoundButton from '@/shared/components/Button/RoundButton';
 
 const TestPage = () => {
   return (
@@ -8,6 +9,12 @@ const TestPage = () => {
         <Button text="Vlock 쌓으러 가기" />
         <Button text="Vlock 쌓으러 가기" variant="gradient" />
         <Button text="Vlock 쌓으러 가기" bg="bg-red-400" />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <h1>✅ RoundButton.tsx</h1>
+        <RoundButton text="여행 조립하러 떠나기" width={292} />
+        <RoundButton text="여행 시작하기" />
       </div>
     </>
   );
