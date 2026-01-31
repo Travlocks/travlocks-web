7dd82334097cf298859e6ba441f8cc41c6cf8bf4
onebone <jyc00410@gmail.com>
2026-01-31 17:55:17 +0900

    테스트 페이지에 블록 추가

--- src/pages/BlockPage/BlockPage.tsx
+++ src/pages/BlockPage/BlockPage.tsx
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-import { Block } from '@/shared/components/Block/Block';
-import { convertToPolygonConnections, createRectPoints } from '@/shared/components/Block/blockShape';
 import BlockEditor from '@/feature/block/blockBuild/components/BlockEditor';
 
 const BlockPage = () => {
@@ -8,28 +5,6 @@ const BlockPage = () => {
     <div className="flex h-[1091px] w-screen overflow-hidden">
       {/* TODO: 여기다가 여행 타이틀 */}
       <BlockEditor />
-      
-      <Block
-        category="식당"
-        title="향라식당"
-        duration="2시간"
-        icon="food"
-        points={[
-          { x: 0, y: 0 },
-          { x: 160, y: 0 },
-          { x: 160, y: 160 },
-          { x: 320, y: 160 },
-          { x: 320, y: 320 },
-          { x: 0, y: 320 },
-        ]}
-        connections={[
-          { edgeIndex: 0, type: 'plug', align: 'start' },
-          { edgeIndex: 3, type: 'socket', align: 'end' },
-          { edgeIndex: 4, type: 'socket', align: 'start' },
-          { edgeIndex: 5, type: 'plug', align: 'end' },
-        ]}
-      />
-
     </div>
   );
 };
--- src/shared/ui/icon/registry.ts
+++ src/shared/ui/icon/registry.ts
@@ -1,8 +1,14 @@
 import Arrow from '@/shared/assets/splash/icon-arrow.svg?react';
+import Clock from '@/feature/block/blockBuild/assets/edit-icon-clock.svg?react';
+import DragHandle from '@/shared/assets/icon-drag-handle.svg?react';
+import Food from '@/shared/assets/preference/icon-preference-food.svg?react';
 import X from '@/shared/assets/icon-x.svg?react';
 
 export const iconRegistry = {
   arrow: Arrow,
+  clock: Clock,
+  dragHandle: DragHandle,
+  food: Food,
   x: X,
 } as const;
 
