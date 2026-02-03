48c7eed45d0f96b30eb27dd15d65feba3dea739c
moni <muwingky@gmail.com>
2026-02-03 16:30:37 +0900

    Feat: ResultSection/검색 결과 3가지 UI 기본 구현

---
+++ src/feature/search/component/ResultSection/ErrorResult.tsx
@@ -0,0 +1,9 @@
+const ErrorResult = () => {
+  return (
+    <>
+      <h1>ErrorResult</h1>
+    </>
+  );
+};
+
+export default ErrorResult;
---
+++ src/feature/template/TemplateCardSkeleton.tsx
@@ -0,0 +1,5 @@
+const TemplateCardSkeleton = () => {
+  return <div>skeleton</div>;
+};
+
+export default TemplateCardSkeleton;
