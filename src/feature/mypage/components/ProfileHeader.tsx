fe97f5d6d0cf8d0785060196bf90031f77301993
onebone <jyc00410@gmail.com>
2026-02-07 03:40:22 +0900

    상태 카드 디자인 조정

--- src/feature/mypage/components/ProfileHeader.tsx
+++ src/feature/mypage/components/ProfileHeader.tsx
@@ -20,11 +20,11 @@ const ProfileHeader = ({ className }: ProfileHeaderProps) => {
 
         <div className="flex flex-col gap-2">
           <h1 className="h1 text-base-color-0">유저 닉네임</h1>
-          <p className="h5 text-base-color-1">한줄소개 내용이 이곳에 들어갑니다</p>
+          <p className="h5 text-base-color-2">한줄소개 내용이 이곳에 들어갑니다</p>
         </div>
       </div>
 
-      <button className="flex items-center gap-x-1.5 px-6 py-3 border border-base-color rounded-full text-base-color-1 h6 hover:bg-base-color-4 transition-colors">
+      <button className="flex items-center gap-1.25 px-6.25 py-3.25 border border-base-color rounded-[10px] text-base-color-2 h6 hover:bg-base-color-4 transition-colors mt-13">
         <SettingsIcon />
         계정 설정
       </button>
