2e0eb382d230ad2f98ecbdf2aea02cc57faf1c63
jinhyo <kjhyo0220@gmail.com>
2026-02-06 17:05:05 +0900

    Design: 타임라인 최소 높이 설정 (#51)

--- src/feature/block/blockTimeLine/BlockTimeLine.tsx
+++ src/feature/block/blockTimeLine/BlockTimeLine.tsx
@@ -22,12 +22,12 @@ const BlockTimeLine = ({ setLevel }: BlockTimeLineProps) => {
   const { blocksByDay } = useBlockEditor(); // 날짜별 블록
 
   return (
-    <div className="relative w-full h-full bg-[#F8FAFC]">
+    <div className="relative w-full h-full bg-[#F8FAFC] flex flex-col">
       <div className="h-[79px] bg-base-color-6 border-b border-base-color py-[23px] px-[30px] flex items-center">
         <p className="h4">Timeline</p>
       </div>
 
-      <div className="py-[65px] px-[49px] flex gap-[40px] overflow-scroll">
+      <div className="flex-1 py-[65px] px-[49px] flex gap-[40px] overflow-scroll min-h-[70dvh]">
         {Array.from({ length: days }, (_, i) => (
           <DayCard key={i} day={i + 1} items={blocksByDay[i + 1]} onClick={() => setLevel('editor')} />
         ))}
