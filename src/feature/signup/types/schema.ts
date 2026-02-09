0e8cac010f6cfe4c391fcf043e33c54bd2308d97
jinhyo <kjhyo0220@gmail.com>
2026-02-09 15:24:55 +0900

    Fix: 회원가입 스키마 타입 수정 (#64)

--- src/feature/signup/types/schema.ts
+++ src/feature/signup/types/schema.ts
@@ -3,17 +3,35 @@ import z from 'zod';
 export const schema = z
   .object({
     email: z.string().email({ message: '올바르지 않은 이메일 형식입니다.' }),
+
     code: z.string(),
+
     password: z
       .string()
       .min(8)
       .regex(/^(?=.*[A-Za-z])(?=.*\d).+$/),
+
     passwordCheck: z.string(),
+
     nickname: z
       .string()
       .min(2, { message: '닉네임은 2자 이상 ~ 10자 이하로 입력해주세요' })
       .max(10, { message: '닉네임은 2자 이상 ~ 10자 이하로 입력해주세요' }),
+
     verificationId: z.string(),
+
+    signupToken: z.string(),
+
+    consents: z.array(
+      z.object({
+        policyId: z.number(),
+        agreed: z.boolean(),
+      }),
+    ),
+
+    preferredTravelStyleIds: z.array(z.number()),
+
+    preferredTravelThemeIds: z.array(z.number()),
   })
   .refine((data) => data.password === data.passwordCheck, {
     message: '비밀번호가 일치하지 않습니다',
