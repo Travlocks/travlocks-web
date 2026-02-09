08a57ad55ef3858a3c442390d35fcecb548e0103
jinhyo <kjhyo0220@gmail.com>
2026-02-09 17:06:25 +0900

    Feat: 회원가입 완료 인터랙션 데이터 연결 (#64)

--- src/feature/signup/hooks/mutations/usePostSignup.ts
+++ src/feature/signup/hooks/mutations/usePostSignup.ts
@@ -1,9 +1,11 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, type UseMutationOptions } from '@tanstack/react-query';
 import { postSignup } from '../../apis/auth';
+import type { RequestSignupDto, ResponseSignupDto } from '../../types/auth';
 
-function usePostSignup() {
+function usePostSignup(options?: UseMutationOptions<ResponseSignupDto, Error, RequestSignupDto>) {
   return useMutation({
     mutationFn: postSignup,
+    ...options,
   });
 }
 
