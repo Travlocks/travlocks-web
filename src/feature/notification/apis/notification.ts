763b6d6d502ade1ce59bd814ea1bcb18e932762d
jinhyo <kjhyo0220@gmail.com>
2026-02-11 01:36:36 +0900

    Feat: 알림 리스트 조회 api 연결 (#76)

--- src/feature/notification/apis/notification.ts
+++ src/feature/notification/apis/notification.ts
@@ -1,9 +1,24 @@
 import { axiosInstance } from '@/shared/apis/axios';
-import type { ResponseNotificationsDto } from '../types/notification';
+import type { ResponseGetNotificationDto, ResponseNotificationsDto } from '../types/notification';
 
 // SSE용 토큰 생성 및 쿠키 저장
 export const postSSEToken = async (): Promise<ResponseNotificationsDto> => {
   const { data } = await axiosInstance.post('/notifications/sse-token');
+
+  return data;
+};
+
+// 알림 리스트 조회
+export const getNotificationList = async (
+  cursor: string | undefined,
+  size: number,
+): Promise<ResponseGetNotificationDto> => {
+  const { data } = await axiosInstance.get(`/notifications`, {
+    params: {
+      cursor,
+      size,
+    },
+  });
 
   return data;
 };
--- src/shared/components/Navbar/NavbarNotificationModal.tsx
+++ src/shared/components/Navbar/NavbarNotificationModal.tsx
@@ -1,35 +1,7 @@
+import useGetNotificationList from '@/feature/notification/hook/useQuery/useGetNotificationList';
 import ShuffleIcon from '@assets/Navbar/icon-shuffle.svg?react';
-import { useEffect, useRef, useState, type SetStateAction } from 'react';
-
-const LISTS = [
-  {
-    id: 1,
-    content: (
-      <>
-        <span className="font-[600]">여행러버</span> 님이 내 템플릿을 리믹스했습니다
-      </>
-    ),
-    time: '3분 전',
-  },
-  {
-    id: 2,
-    content: (
-      <>
-        <span className="font-[600]">조아</span> 님이 내 템플릿을 리믹스했습니다
-      </>
-    ),
-    time: '5분 전',
-  },
-  {
-    id: 3,
-    content: (
-      <>
-        <span className="font-[600]">2026</span> 님이 내 템플릿을 리믹스했습니다
-      </>
-    ),
-    time: '5분 전',
-  },
-];
+import { useInView } from 'react-intersection-observer';
+import { useEffect, useRef, type SetStateAction } from 'react';
 
 interface NavbarNotificationModal {
   setShowNotificationModal: React.Dispatch<SetStateAction<boolean>>;
@@ -38,10 +10,15 @@ interface NavbarNotificationModal {
 
 const NavbarNotificationModal = ({ setShowNotificationModal, alarmRef }: NavbarNotificationModal) => {
   const modalRef = useRef<HTMLDivElement | null>(null);
-  const [data, setData] = useState(LISTS);
+
+  const { data: notificationData, fetchNextPage, hasNextPage, isFetching } = useGetNotificationList();
+  const notificationsList = notificationData?.pages.map((page) => page.data.notifications).flat() ?? [];
+  const hasNotifications = notificationsList.length > 0;
+
+  const { ref, inView } = useInView({ threshold: 0 });
 
   const handleDelete = () => {
-    setData([]);
+    // setData([]);
   };
 
   useEffect(() => {
@@ -60,7 +37,13 @@ const NavbarNotificationModal = ({ setShowNotificationModal, alarmRef }: NavbarN
     return () => {
       document.removeEventListener('mousedown', handleOutside);
     };
-  }, [setShowNotificationModal]);
+  }, [setShowNotificationModal, alarmRef]);
+
+  useEffect(() => {
+    if (inView && !isFetching && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, isFetching, hasNextPage, fetchNextPage]);
 
   return (
     <div
@@ -68,21 +51,34 @@ const NavbarNotificationModal = ({ setShowNotificationModal, alarmRef }: NavbarN
       className="absolute top-[85px] right-0 w-[585px] bg-white rounded-[30px] px-[34px] pt-[27px] pb-[25px] shadow-[0_1px_20px_0_rgba(0,0,0,0.10)]">
       <div className="flex justify-between pb-[27px] border-b border-base-color">
         <p className="h4">알림</p>
-        <p onClick={handleDelete} className="h6 font-[500] text-base-color-2">
-          전체 삭제
-        </p>
+        {hasNotifications && (
+          <p onClick={handleDelete} className="h6 font-[500] text-base-color-2">
+            전체 삭제
+          </p>
+        )}
       </div>
 
       <div className="flex flex-col gap-[20px] py-[25px] overflow-y-auto h-[239px]">
-        {data.map((list) => (
-          <div key={list.id} className="flex gap-[12px] items-center">
-            <div className="size-[40px] rounded-full bg-[#E0E7FF] flex justify-center items-center">
-              <ShuffleIcon />
+        {notificationsList.map((list) => (
+          <div key={list.notificationId} className="flex flex-col">
+            <div className="flex gap-[12px] items-center">
+              <div className="size-[40px] rounded-full bg-[#E0E7FF] flex justify-center items-center">
+                <ShuffleIcon />
+              </div>
+              <p className="h6 font-[300]">
+                <span className="font-[600]">{list.actorNickname}</span>님이 내 템플릿을 리믹스했습니다
+              </p>
             </div>
-            <p className="h6 font-[300]">{list.content}</p>
-            <p className="mt-[25.5px] flex-1 text-end text-base-color-2">{list.time}</p>
+
+            <p className="text-end text-base-color-2 mt-[-4px]">{list.timeAgo}</p>
           </div>
         ))}
+
+        {!hasNotifications && (
+          <p className="h6 font-[400] flex items-center justify-center flex-1">아직 새로운 알림이 없어요.</p>
+        )}
+
+        <div ref={ref}></div>
       </div>
     </div>
   );
