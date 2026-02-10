7cdaf9a588f1349a2dc7407bbca4f755c744c67d
jinhyo <kjhyo0220@gmail.com>
2026-02-10 12:30:08 +0900

    Refactor: enabled 조건 추가 (#76)

--- src/feature/user/hooks/queries/useGetMypage.ts
+++ src/feature/user/hooks/queries/useGetMypage.ts
@@ -1,11 +1,15 @@
 import { QUERY_KEY } from '@/shared/constants/key';
 import { useQuery } from '@tanstack/react-query';
 import { getMypage } from '../../apis/user';
+import { useAuth } from '@/shared/hooks/useAuth';
 
 function useGetMyPage() {
+  const { isAuthenticated } = useAuth();
+
   return useQuery({
     queryKey: [QUERY_KEY.member],
     queryFn: getMypage,
+    enabled: isAuthenticated,
   });
 }
 
