027c2f8a2d82197bacda339ea3834db01460e789
onebone <jyc00410@gmail.com>
2026-02-12 00:27:52 +0900

    계정 설정에서도 최대 크기 설정

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,11 +1,11 @@
 import Dashboard from '@/feature/mypage/components/Dashboard';
-import clsx from 'clsx';
 
 const MyPage = () => {
   return (
-    // TODO: bg 색 확인?
-    <div className={clsx('bg-[#F8FAFC] px-92')}>
-      <Dashboard />
+    <div className="bg-[#F8FAFC]">
+      <div className="mx-auto w-full max-w-300">
+        <Dashboard />
+      </div>
     </div>
   );
 };
