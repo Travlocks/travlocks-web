6b0dbca69c9d7a5fa0fc917ee6b413f3f65ccee7
jinhyo <kjhyo0220@gmail.com>
2026-02-17 04:20:22 +0900

    Fix: 타임라인 DAY 등록한 일수만큼만 나오도록 (#108)

--- src/feature/block/blockTimeLine/BlockTimeLine.tsx
+++ src/feature/block/blockTimeLine/BlockTimeLine.tsx
@@ -1,25 +1,17 @@
 import type { Dispatch, SetStateAction } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import { useBlockEditor } from '../blockBuild/hooks/useBlockEditor';
 import DayCard from './components/DayCard';
 import RecommendCard from './components/RecommendCard';
-
-const TravelData = {
-  destinationCityIds: [301, 302],
-  trip: {
-    days: 5,
-    nights: 4,
-  },
-  transportTypes: ['WALK', 'TRANSIT'],
-  travelThemeIds: [1, 2, 3],
-};
 
 interface BlockTimeLineProps {
   setLevel: Dispatch<SetStateAction<'timeline' | 'editor'>>;
 }
 
 const BlockTimeLine = ({ setLevel }: BlockTimeLineProps) => {
-  const { days } = TravelData.trip; // 여행 일수
   const { blocksByDay, actions } = useBlockEditor(); // 날짜별 블록
+  const location = useLocation();
 
   return (
     <div className="relative w-full h-full bg-[#F8FAFC] flex flex-col">
@@ -28,7 +20,7 @@ const BlockTimeLine = ({ setLevel }: BlockTimeLineProps) => {
       </div>
 
       <div className="flex-1 py-[65px] px-[49px] flex gap-[40px] overflow-scroll min-h-[70dvh]">
-        {Array.from({ length: days }, (_, i) => (
+        {Array.from({ length: location.state.data.dayCount }, (_, i) => (
           <DayCard
             key={i}
             day={i + 1}
