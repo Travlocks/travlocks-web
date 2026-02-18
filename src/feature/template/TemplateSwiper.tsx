83a780b82cd8dbd460605dbbc9f69b05fcdb5dba
jinhyo <kjhyo0220@gmail.com>
2026-02-18 22:10:50 +0900

    Fix: 클린업함수 추가 (#110)

--- src/feature/template/TemplateSwiper.tsx
+++ src/feature/template/TemplateSwiper.tsx
@@ -58,6 +58,10 @@ const TemplateSwiper = ({ cards, type }: TemplateSwiperProps) => {
 
     onSelect();
     emblaApi.on('select', onSelect);
+
+    return () => {
+      emblaApi.off('select', onSelect);
+    };
   }, [emblaApi, onSelect]);
 
   return (
